45514ba2ab5a20f88cb666af67ca92b910349eaf
avigok <gh@avigok.com>
2022-02-22 18:23:20 +0100

    update helper script, docs for 5.10 BSP, and gitignore

--- scripts/image_build.sh
+++ scripts/image_build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# My boards :: imx6ull14x14evk
+# My boards :: imx6ull14x14evk , imx6qsabresd
 # My boards :: imx8qmmek , imx8mqevk , imx8mnevk , imx8mmevk , imx8mpevk
 
 TARGET_DEVICE=imx8mmevk
 GRPH_FRONTEND=xwayland
-DISTRO=fsl-imx-$GRPH_FRONTEND MACHINE=$TARGET_DEVICE source imx-setup-release.sh -b $TARGET_DEVICE-$GRPH_FRONTEND
+DISTRO=fsl-imx-$GRPH_FRONTEND MACHINE=$TARGET_DEVICE source imx-setup-release.sh -b build-$GRPH_FRONTEND
 
 # Add essential tools to the build
 sh ./02_pkg_overlay.sh
