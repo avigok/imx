e2f2bb556de517026cb8101a7465bedb9d35dc1f
Avinash Gokhale <33357350+avigok@users.noreply.github.com>
2022-06-09 20:11:50 +0200

    update overlay to handle yocto 3.4+ (Honister) update. Needed for 5.15.y imx builds.

--- scripts/overlay.sh
+++ scripts/overlay.sh
@@ -5,20 +5,17 @@ for pkg in pciutils iperf3 hostapd wpa-supplicant\
 	 bridge-utils htop dropbear dnsmasq\
 	 iptables sysstat git python3\
 	 kernel-devsrc binutils packagegroup-core-buildessential\
-	 iw dtc openssl libssl libnl libnl-genl rsync
-do
-	echo IMAGE_INSTALL_append += "\" $pkg\"" >> conf/local.conf
-done
-
-# pulseaudio packages for bluetooth
-for pkg in pulseaudio-server\
+	 iw dtc openssl libssl libnl libnl-genl rsync\
+	 pulseaudio-server\
 	 pulseaudio-module-bluetooth-discover\
 	 pulseaudio-module-bluetooth-policy\
 	 pulseaudio-module-bluez5-device\
 	 pulseaudio-module-bluez5-discover
 do
+	# Applicable from Yocto 3.4+ (Honister+)
+	# ---------------------------------------------------
 	# if conf/local.conf has CONF_VERSION = "2",
 	# adapt IMAGE_INSTALL_append to IMAGE_INSTALL:append
-	# Applicable from Yocto 3.4+ (Honister or above..)
+	# ---------------------------------------------------
 	echo IMAGE_INSTALL_append += "\" $pkg\"" >> conf/local.conf
 done
